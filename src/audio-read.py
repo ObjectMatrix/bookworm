12027b679729cba2af40418df6b1d23ebbf278a9
Ahm A <asad.gooogle@gmail.com>
2020-11-24 12:01:24 -0500

    Create audio-read.py
    read pdf

---
+++ src/audio-read.py
@@ -0,0 +1,19 @@
+import pyttsx3
+import PyPDF2
+
+name = pdfFileName = 'god.pdf'
+book = open(pdfFileName, 'rb')
+reader = PyPDF2.PdfFileReader(book)
+totalPages = reader.numPages
+print(totalPages)
+engine = pyttsx3.init()
+
+voiceRate = 180
+engine.setProperty('rate', voiceRate)
+
+startPage = 11
+for num in range(startPage, totalPages):
+  page = reader.getPage(num)
+  text = page.extractText()
+  engine.say(text)
+  engine.runAndWait()
